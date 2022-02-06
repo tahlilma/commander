ad27c992e0ff3888478760df5a3424758c16bcd1
Tahlil <sucmanuts420@gmail.com>
2022-02-06 13:41:57 +0600

    added a guide

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,7 @@
 module.exports = {
   name: "ping",
-  alt: "p",
-  description: "Just replies with hello.",
-  handler: (message) => {
-    message.channel.send("Hello World");
+  alt: "p", 
+  handler: (message) => { 
+    message.channel.send("pong");
   },
 };
